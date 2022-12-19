c19031cd071be035ad4d908917758d76c84b3952
abzal <abzal008@mail.ru>
2022-12-20 04:42:45 +0600

    .

--- src/family-space/dto/create-family-space.dto.ts
+++ src/family-space/dto/create-family-space.dto.ts
@@ -6,6 +6,8 @@ export class CreateFamilySpaceDto {
 
   @ApiProperty({example: 'Family', description: 'title of the family'})
   readonly title: string;
+  @ApiProperty({example: '234', description: 'status of space'})
+  readonly status: string
 
   @ApiProperty({example: '1234', description: 'pass of the family'})
   readonly password: string
